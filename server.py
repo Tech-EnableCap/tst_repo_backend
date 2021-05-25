2c194f312dbada3c98672c2a85c6b3fbe465ab9c
Anish Majumdar <tech@enablecap.in>
2021-05-25 19:25:02 +0530

    Test Deploy Back-End

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+app=Flask(__name__)
+CORS(app)
+@app.route("/",methods=["POST","GET"])
+@cross_origin(supports_credentials=True)
+def exp():
+	return jsonify({'h': 'helloWorld'})
+
+	app.run()
